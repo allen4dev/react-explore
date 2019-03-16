a345757bdd19267ad0a5db60e86837007fe712eb
allen4dev <alanaliagadev@gmail.com>
2019-03-16 00:44:25 -0400

    [emotion] first commit

---
+++ emotion/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './App';
+
+function renderApp() {
+  render(<App />, document.getElementById('root'));
+}
+
+renderApp();
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    renderApp();
+  });
+}
