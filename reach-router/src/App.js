d0f7c2fc9ef3bf4e397ea1cbf2e51f62be655bc7
allen4dev <alanaliagadev@gmail.com>
2019-03-16 02:53:34 -0400

    [reach-router][Add] basic example

--- reach-router/src/App.js
+++ reach-router/src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Router, Link } from '@reach/router';
+
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
     <div>
-      <h1>Reach Router</h1>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="about">About</Link>
+      </nav>
+
+      <Router>
+        <Home path="/" />
+        <About path="about" />
+      </Router>
     </div>
   );
 }
