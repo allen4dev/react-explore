08e760b54c3c3ac6f75291a29f727d784bdf6be7
allen4dev <alanaliagadev@gmail.com>
2019-03-16 02:43:41 -0400

    [reach-router] first commit

---
+++ reach-router/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'client.js',
+  },
+  resolve: {
+    modules: ['node_modules', path.join(__dirname, 'src')],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, './public'),
+    hot: true,
+    // hotOnly: true,
+    historyApiFallback: true,
+    overlay: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'eslint-loader',
+        exclude: /node_modules/,
+        enforce: 'pre',
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
