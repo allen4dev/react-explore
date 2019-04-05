e9124866b0d1839ba5896cd8b93e987c7f86349b
allen4dev <alanaliagadev@gmail.com>
2019-04-05 16:55:24 -0400

    [hooks][Add] Searching by a query

--- hooks/src/playgrounds/useEffect/index.js
+++ hooks/src/playgrounds/useEffect/index.js
@@ -5,12 +5,25 @@ import axios from 'axios';
 function Fetch() {
   const [servants, setServants] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [query, setQuery] = useState('');
+  const [search, setSearch] = useState('');
+
+  const handleSearchChange = ({ target: { value } }) => setQuery(value);
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    setSearch(query);
+
+    setQuery('');
+  };
 
   useEffect(() => {
     async function fetchServants() {
       setLoading(true);
 
-      const { data } = await axios.get('http://localhost:3000/servants');
+      const { data } = await axios.get(
+        `http://localhost:3000/servants?name_like=${search}`,
+      );
 
       setServants(data);
 
@@ -18,15 +31,30 @@ function Fetch() {
     }
 
     fetchServants();
-  }, []);
-
-  if (loading) return <div>Loading...</div>;
+  }, [search]);
 
   return (
     <section>
-      {servants.map(servant => (
-        <div key={servant.id}>{servant.name}</div>
-      ))}
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Search servant"
+          value={query}
+          onChange={handleSearchChange}
+        />
+
+        <button type="submit">Search</button>
+      </form>
+
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <section className="servants">
+          {servants.map(servant => (
+            <div key={servant.id}>{servant.name}</div>
+          ))}
+        </section>
+      )}
     </section>
   );
 }
